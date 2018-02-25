9a0bec10453da73f3be8a26145e3d442bdefe4ee
Nightingale <kevin.solovjov@gmail.com>
2018-02-25 22:07:11 +0100

    Switched Static index to mustache

--- app.js
+++ app.js
@@ -4,18 +4,25 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mustacheExpress = require('mustache-express');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// Make it possible to access a session object in Mustache templates
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', mustacheExpress()) // Same as file extensions (*.html)
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -43,7 +50,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
 
