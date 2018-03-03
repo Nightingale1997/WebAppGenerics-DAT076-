c36a1c6b2be2834099efbd99f41e8218c3c966df
desembr <sensati9n@gmail.com>
2018-03-03 11:48:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var exphbs = require('express-validator');
 var mustacheExpress = require('mustache-express');
 var expressValidator = require('express-validator');
-var flash = require('connect-flash');
 var session = require('express-session');
 var MySQLStore = require('express-mysql-session')(session);
 var passport = require('passport');
