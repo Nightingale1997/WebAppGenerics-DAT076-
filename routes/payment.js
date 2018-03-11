6c3e1cc5ceb9bf0ccd27e5ca980b3cd804e0da47
Emir Zivcic <emir-s@hotmail.se>
2018-03-11 21:27:16 +0100

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -8,6 +8,7 @@ var bcrypt = require('bcryptjs');
 var Product = require('../models/Product');
 var Mustache = require('mustache');
 
+
 function loggedIn(req, res, next){
     if(req.user)
         next();
@@ -18,6 +19,7 @@ function loggedIn(req, res, next){
 router.get('/', loggedIn, function (req, res) {
     res.render('payment');
 });
+
 router.post('/', function(req, res){
    const name = req.body.name;
    const address = req.body.address;
@@ -25,15 +27,22 @@ router.post('/', function(req, res){
    const expMo = req.body.expMo;
    const expYear = req.body.expYear;
    const cvc = req.body.cvc;
+   var cartID;
+   const usr = req.user.userID;
 
-   mysql.query('INSERT INTO purchase (PurchaseID, AccountID, ShoppingCartID, Address, CustomerName) VALUES(?, ?, ?, ?, ?)',
-       ['5', req.user.userID, '10', address, name], function(req, res){
 
+   mysql.query('INSERT INTO shoppingcart ( AccountID, Quantity, TotalPrice) VALUES (?, ?, ?)',
+       [req.user.userID, 10, 5], function(req,res){
     });
 
+   mysql.query('SELECT MAX(ShoppingCartID) AS latestCart FROM shoppingcart', function(req,res){
+       cartID = res[0].latestCart;
+       mysql.query('INSERT INTO purchase (AccountID, ShoppingCartID, Address, CustomerName) VALUES(?, ?, ?, ?)',
+           [usr, cartID, address, name], function(req, res){
+           });
+    });
+
+   res.render('index');
 });
-
-
-
 
 module.exports = router;
