c364548506eccfb0d6829b9f7a147fc3b60486ce
Emir Zivcic <emir-s@hotmail.se>
2018-03-11 20:02:43 +0100

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -18,14 +18,18 @@ function loggedIn(req, res, next){
 router.get('/', loggedIn, function (req, res) {
     res.render('payment');
 });
-
-router.post('/payment', function(req, res){
-    /*
+router.post('/', function(req, res){
    const name = req.body.name;
-   const Address = req.body.address;
+   const address = req.body.address;
+   const cardNo = req.body.cardNo;
+   const expMo = req.body.expMo;
+   const expYear = req.body.expYear;
+   const cvc = req.body.cvc;
 
-   console.log(address);
-   */
+   mysql.query('INSERT INTO purchase (PurchaseID, AccountID, ShoppingCartID, Address, CustomerName) VALUES(?, ?, ?, ?, ?)',
+       ['5', req.user.userID, '10', address, name], function(req, res){
+
+    });
 
 });
 
